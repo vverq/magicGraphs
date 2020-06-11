b8ef52c6122e56930fcf76c93e2f25e33dea7f72
vverq <18631869w@gmail.com>
2020-06-12 00:41:44 +0500

    fixed drawing graph (now there is arc instead of straight lines and added arrows)

--- src/main/java/gui/GraphVisualizer.java
+++ src/main/java/gui/GraphVisualizer.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GraphVisualizer extends JPanel {
-
     private Graph graph;
 
     GraphVisualizer(Graph graph) {
@@ -39,11 +38,9 @@ public class GraphVisualizer extends JPanel {
         }
         var adjacencyMatrix = graph.getAdjacencyMatrix();
         for (var i = 0; i < n; i++) {
-            for (var j = 0; j < n; j++) {
-                if (i == j)
-                    continue;
+            for (var j = i+1; j < n; j++) {
                 if (adjacencyMatrix[i][j]) {
-                        int d = (int)Math.sqrt((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]));
+                        int d = (int) Math.sqrt((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]));
                         int ax = x[i];
                         int ay = y[i];
                         int bx = x[j];
@@ -54,37 +51,124 @@ public class GraphVisualizer extends JPanel {
                         int Y = Math.min(ay, by);
                         int width = Math.abs(ax - bx);
                         int height = Math.abs(ay - by);
-                        if (deltaX <= 0 && deltaY <=0) {
-                            g2d.drawArc(X, Y - height, width * 2, height * 2, 180, 90);
-                            g2d.setColor(Color.green);
-                            int l = 8;
-                            int abx = (int)((double)(bx - ax + (width/2)) / d * l);
-                            int aby = (int)((double)(by - ay - (width/2) )/ d * l);
-                            int abx1 = aby;
-                            int aby1 = -abx;
-                            g.drawPolygon(new int [] {bx, bx-abx1-3*abx, bx+abx1-3*abx}, new int [] {by, by-aby1-3*aby, by+aby1-3*aby}, 3);
+                        if (deltaX == 0 || deltaY == 0){
+                            if (deltaX == 0){
+                                width = d/4;
+                                height = d;
+                                g2d.drawArc(X - width/2, Y, width, height, 90, 180);
+                                g2d.setColor(Color.green);
+                                int l = 8;
+                                int abx = (int)((double)(-bx + ax + (width*8)) / d * l/4);
+                                int aby = (int)((double)(-by + ay - (width*8))/ d * l/4);
+                                int abx1 = aby;
+                                int aby1 = -abx;
+                                g.fillPolygon(new int [] {bx, bx-abx1-3*abx, bx+abx1-3*abx}, new int [] {by, by-aby1-3*aby, by+aby1-3*aby}, 3);
+                            }
+                            else
+                            {
+                                width = d;
+                                height = d/4;
+                                g2d.drawArc(X, Y - height/2, width, height, 0, 180);
+                                g2d.setColor(Color.green);
+                                int l = 8;
+                                int abx = (int)((double)(bx - ax + (height*8)) / d * l/4);
+                                int aby = (int)((double)(by - ay - (height*8))/ d * l/4);
+                                int abx1 = aby;
+                                int aby1 = -abx;
+                                g.fillPolygon(new int [] {bx, bx-abx1+3*abx, bx+abx1+3*abx}, new int [] {by, by-aby1+3*aby, by+aby1+3*aby}, 3);
+                            }
                             g2d.setColor(Color.black);
                         }
                         else {
-                            g2d.drawArc(X - width, Y - height, width * 2, height * 2, 0, -90);
+                            if (deltaX < 0 && deltaY < 0 || deltaX > 0 && deltaY > 0) {
+                                g2d.drawArc(X - width, Y, width * 2, height * 2, 0, 90);
+                                g2d.setColor(Color.green);
+                                int l = 4;
+                                int abx = (int) ((double) (-bx + ax + (width / 2)) / d * l);
+                                int aby = (int) ((double) (-by + ay - (width / 2)) / d * l);
+                                int abx1 = aby;
+                                int aby1 = -abx;
+                                g.fillPolygon(new int[]{bx, bx - abx1 + 3 * abx, bx + abx1 + 3 * abx}, new int[]{by, by - aby1 + 3 * aby, by + aby1 + 3 * aby}, 3);
+                                g2d.setColor(Color.black);
+                            } else {
+                                g2d.drawArc(X - width, Y - height, width * 2, height * 2, 0, -90);
+                                g2d.setColor(Color.green);
+                                int l = 4;
+                                int abx = (int) ((double) (ax - bx + (width / 2)) / d * l);
+                                int aby = (int) ((double) (ay - by + (width / 2)) / d * l);
+                                int abx1 = aby;
+                                int aby1 = -abx;
+                                g.fillPolygon(new int[]{bx, bx + abx1 + 3 * abx, bx - abx1 + 3 * abx}, new int[]{by, by + aby1 + 3 * aby, by - aby1 + 3 * aby}, 3);
+                                g2d.setColor(Color.black);
+                            }
+                        }
+               }
+                if (adjacencyMatrix[j][i])
+                {
+                    int d = (int) Math.sqrt((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]));
+                    int ax = x[j];
+                    int ay = y[j];
+                    int bx = x[i];
+                    int by = y[i];
+                    int deltaX = bx - ax;
+                    int deltaY = by - ay;
+                    int X = Math.min(ax, bx);
+                    int Y = Math.min(ay, by);
+                    int width = Math.abs(ax - bx);
+                    int height = Math.abs(ay - by);
+                    if (deltaX == 0 || deltaY == 0) {
+                        if (deltaX == 0) {
+                            width = d/4;
+                            height = d;
+                            g2d.drawArc(X - width/2, Y, width, height, -90, 180);
                             g2d.setColor(Color.green);
                             int l = 8;
-                            int abx = (int)((double)(ax - bx + (width/2)) / d * l);
-                            int aby = (int)((double)(ay - by + (width/2)) / d * l);
+                            int abx = (int)((double)(bx - ax + (width*8)) / d * l/4);
+                            int aby = (int)((double)(by - ay - (width*8))/ d * l/4);
                             int abx1 = aby;
                             int aby1 = -abx;
-                            g.drawPolygon(new int [] {bx, bx+abx1+3*abx, bx-abx1+3*abx}, new int [] {by, by+aby1+3*aby, by-aby1+3*aby}, 3);
+                            g.fillPolygon(new int [] {bx, bx-abx1+3*abx, bx+abx1+3*abx}, new int [] {by, by-aby1+3*aby, by+aby1+3*aby}, 3);
+                        }
+                        else {
+                            width = d;
+                            height = d/4;
+                            g2d.drawArc(X, Y - height/2, width, height, 180, 180);
+                            g2d.setColor(Color.green);
+                            int l = 8;
+                            int abx = (int)((double)(-bx + ax + (height*8)) / d * l/4);
+                            int aby = (int)((double)(-by + ay - (height*8))/ d * l/4);
+                            int abx1 = aby;
+                            int aby1 = -abx;
+                            g.fillPolygon(new int [] {bx, bx-abx1-3*abx, bx+abx1-3*abx}, new int [] {by, by-aby1-3*aby, by+aby1-3*aby}, 3);
+                        }
+                        g2d.setColor(Color.black);
+                    }
+                    else {
+                        if (deltaX < 0 && deltaY < 0 || deltaX > 0 && deltaY > 0) {
+                            g2d.drawArc(X, Y - height, width * 2, height * 2, 180, 90);
+                            g2d.setColor(Color.green);
+                            int l = 4;
+                            int abx = (int) ((double) (bx - ax + (width / 2)) / d * l);
+                            int aby = (int) ((double) (by - ay - (width / 2)) / d * l);
+                            int abx1 = aby;
+                            int aby1 = -abx;
+                            g.fillPolygon(new int[]{bx, bx - abx1 - 3 * abx, bx + abx1 - 3 * abx}, new int[]{by, by - aby1 - 3 * aby, by + aby1 - 3 * aby}, 3);
                             g2d.setColor(Color.black);
                         }
-                        //такая же дуга, но в другую сторону:
-                        //g2d.setColor(Color.blue);
-                        //g2d.drawArc(X, Y, width*2, height*2, 90, 90);
-               }
+                        else {
+                            g2d.drawArc(X, Y, width * 2, height * 2, 90, 90);
+                            g2d.setColor(Color.green);
+                            int l = 4;
+                            int abx = (int) ((double) (-ax + bx + (width / 2)) / d * l);
+                            int aby = (int) ((double) (-ay + by + (width / 2)) / d * l);
+                            int abx1 = aby;
+                            int aby1 = -abx;
+                            g.fillPolygon(new int[]{bx, bx + abx1 - 3 * abx, bx - abx1 - 3 * abx}, new int[]{by, by + aby1 - 3 * aby, by - aby1 - 3 * aby}, 3);
+                            g2d.setColor(Color.black);
+                        }
+                    }
+                }
             }
         }
-    }
-
-    private double getAngle(int x2, int y2, int x1, int y1) {
-        return Math.atan2(y1 - y2, x2 - x1);
     }
 }
