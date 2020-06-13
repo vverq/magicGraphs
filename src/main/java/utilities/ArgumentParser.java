6e642d87f9f2c3c935f0d7cb0f06a3eb3089e354
vverq <18631869w@gmail.com>
2020-06-13 15:05:12 +0500

    fixed ArgumentParser (now there are enums with each param)

--- src/main/java/utilities/ArgumentParser.java
+++ src/main/java/utilities/ArgumentParser.java
@@ -3,15 +3,21 @@ package utilities;
 import java.util.HashMap;
 
 public class ArgumentParser {
+    enum typeGraph {
+        directed,
+        undirected
+    }
+
+    enum algorithms {
+        DFS,
+        BFS
+    }
+
     public static HashMap<String, String> parseArguments(String[] args) {
         HashMap<String, String> arguments = new HashMap<>();
-        if (args[0].equals("directed")) {
-            arguments.put("type", "directed");
-        }
-        else if (args[0].equals("undirected")) {
-            arguments.put("type", "undirected");
-        }
-        else {
+        try {
+            arguments.put("type", typeGraph.valueOf(args[0]).toString());
+        } catch (IllegalArgumentException ex) {
             System.out.println("Please, check examples in README.md: " +
                     "there are only two modes (<directed> and <undirected>)");
             System.exit(1);
@@ -26,10 +32,9 @@ public class ArgumentParser {
                 System.exit(2);
             }
         }
-        if (args[2].equals("DFS") || args[2].equals("BFS")) {
-            arguments.put("algorithms", args[2]);
-        }
-        else {
+        try {
+            arguments.put("algorithms", algorithms.valueOf(args[2]).toString());
+        } catch (IllegalArgumentException ex) {
             System.out.println("Now you can choose only two algorithms: <DFS> or <BFS>");
             System.exit(3);
         }
